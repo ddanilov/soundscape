103c6f2b2e45acf9b49110a9fa4555583c815ce6
Denis Danilov <ddanilov@users.noreply.github.com>
2022-06-16 18:41:55 +0200

    reformat CMake command names to lower-case

--- cmake/ClangFormat.cmake
+++ cmake/ClangFormat.cmake
@@ -1,6 +1,6 @@
 # let's use CPPLINT property to run clang-format check
-FIND_PROGRAM(CLANGFORMAT clang-format)
-IF(CLANGFORMAT)
-  MESSAGE(STATUS "Setup clang-format check")
-  SET(CMAKE_CXX_CPPLINT "${CLANGFORMAT};--dry-run;--Werror")
-ENDIF()
+find_program(CLANGFORMAT clang-format)
+if(CLANGFORMAT)
+  message(STATUS "Setup clang-format check")
+  set(CMAKE_CXX_CPPLINT "${CLANGFORMAT};--dry-run;--Werror")
+endif()
