38683b9f99f674e34ae9fa0eb0f11314e9f742ae
Denis Danilov <ddanilov@users.noreply.github.com>
2022-08-14 14:53:24 +0200

    make coverage report optional

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -1,27 +1,12 @@
 find_program(GCOVR gcovr)
 
-if(GCOVR AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_BUILD_TYPE STREQUAL "Debug")
-  message(STATUS "setup GCOV coverage report")
+set(GCOVR_COVERAGE_ENABLED OFF)
 
+if(WITH_COVERAGE AND GCOVR AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_BUILD_TYPE STREQUAL "Debug")
+  message(STATUS "setup GCOV coverage report")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
-
-  set(CTEST_COVERAGE ${PROJECT_NAME}_ctest_coverage)
-  add_CUSTOM_TARGET(${CTEST_COVERAGE}
-    COMMAND ctest -C $<CONFIG> $ENV{ARGS} $$ARGS
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
-
-  set(REPORT_BASE "coverage")
-  message(STATUS "${PROJECT_BINARY_DIR}/${REPORT_BASE}")
-  add_custom_command(TARGET ${CTEST_COVERAGE} POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/${REPORT_BASE}
-    COMMAND gcovr
-      --html --html-details
-      --output ${REPORT_BASE}/index.html
-      --exclude-unreachable-branches
-      --exclude-throw-branches
-      --root ${PROJECT_SOURCE_DIR}
-      --object-directory ${PROJECT_BINARY_DIR}
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-    COMMENT "code coverage report by gcovr")
+  set(GCOVR_COVERAGE_ENABLED ON)
+  set(GCOVR_REPORT_BASE "coverage")
+  message(STATUS "coverage report base: ${PROJECT_BINARY_DIR}/${GCOVR_REPORT_BASE}")
 endif()
