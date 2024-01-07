8f2f0f04acaaca8b8ddb00f100e0919cea0773d6
Denis Danilov <ddanilov@users.noreply.github.com>
2024-01-07 23:31:46 +0100

    new code coverage setup

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -1,15 +1,39 @@
-# SPDX-FileCopyrightText: 2022 Denis Danilov
+# SPDX-FileCopyrightText: 2024 Denis Danilov
 # SPDX-License-Identifier: GPL-3.0-only
 
-find_program(GCOVR gcovr)
+option(WITH_COVERAGE "Enable code coverage reports for tests." OFF)
 
-set(GCOVR_COVERAGE_ENABLED OFF)
+if(WITH_COVERAGE)
+  find_program(GCOVR gcovr REQUIRED)
 
-if(WITH_COVERAGE AND GCOVR AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_BUILD_TYPE STREQUAL "Debug")
-  message(STATUS "setup GCOV coverage report")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
-  set(GCOVR_COVERAGE_ENABLED ON)
-  set(GCOVR_REPORT_BASE "coverage")
-  message(STATUS "coverage report base: ${PROJECT_BINARY_DIR}/${GCOVR_REPORT_BASE}")
+  if (NOT (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_BUILD_TYPE STREQUAL "Debug"))
+    message(SEND_ERROR "can't prepare coverage")
+  endif()
+
+  add_compile_options(--coverage)
+  add_link_options(--coverage)
+
+  set(CTEST_COVERAGE_DIR "coverage")
+  message(STATUS "coverage report base: ${PROJECT_BINARY_DIR}/${CTEST_COVERAGE_DIR}")
+
+  set(EXCLUDE_PATTERN ".*tests")
+
+  add_custom_target(coverage
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_BINARY_DIR}/${CTEST_COVERAGE_DIR}
+    COMMAND gcovr
+    --html --html-details
+    --exclude "${EXCLUDE_PATTERN}"
+    --output ${CTEST_COVERAGE_DIR}/index.html
+    --root ${PROJECT_SOURCE_DIR}
+    --object-directory ${PROJECT_BINARY_DIR}
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+
+  add_custom_target(coveralls
+    COMMAND gcovr
+    --coveralls coverage.json
+    --exclude "${EXCLUDE_PATTERN}"
+    --root ${PROJECT_SOURCE_DIR}
+    --object-directory ${PROJECT_BINARY_DIR}
+    WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+
 endif()
