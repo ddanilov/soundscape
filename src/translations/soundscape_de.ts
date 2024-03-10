f918ea13d275a7a091ef54ec5bc3515da0e1242f
Denis Danilov <ddanilov@users.noreply.github.com>
2024-03-10 21:15:27 +0100

    add german translation

---
+++ src/translations/soundscape_de.ts
@@ -0,0 +1,260 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>Help</name>
+    <message>
+        <location filename="../Main.cpp" line="36"/>
+        <source>Load track list from file.</source>
+        <translation>Spuren aus Datei laden.</translation>
+    </message>
+    <message>
+        <location filename="../Main.cpp" line="37"/>
+        <source>path to file</source>
+        <translation>Pfad zur Datei</translation>
+    </message>
+    <message>
+        <location filename="../Main.cpp" line="40"/>
+        <source>Minimize window to tray.</source>
+        <translation>Minimiert im Benachrichtigungsfeld starten.</translation>
+    </message>
+    <message>
+        <location filename="../Main.cpp" line="44"/>
+        <source>Enable tray icon.</source>
+        <translation>Benachrichtigungsfeld aktivieren.</translation>
+    </message>
+    <message>
+        <location filename="../Main.cpp" line="46"/>
+        <source>Disable tray icon.</source>
+        <translation>Benachrichtigungsfeld deaktivieren.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.cpp" line="45"/>
+        <source>Use mouse right-click
+to access application menu</source>
+        <translation>Benutze rechte Maustaste
+für Anwendungsmenü</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="51"/>
+        <location filename="../MainWindow.cpp" line="52"/>
+        <location filename="../MainWindow.cpp" line="110"/>
+        <location filename="../MainWindow.cpp" line="390"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="77"/>
+        <source>Pause playing tracks</source>
+        <translation>aktive Spuren pausieren</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="80"/>
+        <source>Resume paused tracks</source>
+        <translation>pausierte Spuren fortsetzen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="86"/>
+        <source>Add track</source>
+        <translation>Neue Spur</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="89"/>
+        <source>Save track list</source>
+        <translation>Spuren speichern</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="92"/>
+        <source>Load track list</source>
+        <translation>Spuren laden</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="95"/>
+        <source>Examples</source>
+        <translation>Beispiele</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="98"/>
+        <source>Rain and Thunder</source>
+        <translation>Regen und Donner</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="101"/>
+        <source>River</source>
+        <translation>Am Fluss</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="114"/>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="269"/>
+        <source>Show window</source>
+        <translation>Fenster zeigen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="322"/>
+        <source>Save Tracks</source>
+        <translation>Spuren speichern</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="323"/>
+        <location filename="../MainWindow.cpp" line="346"/>
+        <source>couldn&apos;t open file: %1</source>
+        <translation>die Datei %1 konnte nicht geöffnen werden</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="345"/>
+        <source>Load Tracks</source>
+        <translation>Spuren laden</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="377"/>
+        <source>Version: %1</source>
+        <translation>Version: %1</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="379"/>
+        <source>open-source system-tray resident desktop application for playing soundscapes</source>
+        <translation>Desktop Anwendung mit offenem Quellcode für Wiedergabe von Klangschaften</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="382"/>
+        <source>Website: &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
+        <translation>Webseite: &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="383"/>
+        <source>Copyright: © 2022-2024 Denis Danilov and contributors</source>
+        <translation>Copyright: © 2022-2024 Denis Danilov und Mitwirkende</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="384"/>
+        <source>License: GNU General Public License (GPL) version 3</source>
+        <translation>Lizenz: GNU General Public License (GPL) version 3</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="387"/>
+        <source>Qt Version: %1</source>
+        <translation>Qt Version: %1</translation>
+    </message>
+</context>
+<context>
+    <name>PositionLabel</name>
+    <message>
+        <location filename="../PositionLabel.h" line="18"/>
+        <source>%1 s</source>
+        <translation>%1 s</translation>
+    </message>
+</context>
+<context>
+    <name>Status</name>
+    <message>
+        <location filename="../Status.cpp" line="45"/>
+        <source>paused</source>
+        <translation>Pausiert</translation>
+    </message>
+    <message>
+        <location filename="../Status.cpp" line="48"/>
+        <source>playing</source>
+        <translation>Wiedergabe</translation>
+    </message>
+</context>
+<context>
+    <name>TrackControls</name>
+    <message>
+        <location filename="../TrackControls.cpp" line="173"/>
+        <source>Edit Settings</source>
+        <translation>Spur-Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../TrackControls.cpp" line="176"/>
+        <source>Move Up</source>
+        <translation>Spur hoch schieben</translation>
+    </message>
+    <message>
+        <location filename="../TrackControls.cpp" line="179"/>
+        <source>Move Down</source>
+        <translation>Spur runter schieben</translation>
+    </message>
+    <message>
+        <location filename="../TrackControls.cpp" line="182"/>
+        <source>Remove</source>
+        <translation>Spur entfernen</translation>
+    </message>
+</context>
+<context>
+    <name>TrackSettings</name>
+    <message>
+        <location filename="../TrackSettings.cpp" line="91"/>
+        <source>track duration</source>
+        <translation>Spurdauer</translation>
+    </message>
+    <message>
+        <location filename="../TrackSettings.cpp" line="112"/>
+        <source>fade-in</source>
+        <translation>Einblenden</translation>
+    </message>
+    <message>
+        <location filename="../TrackSettings.cpp" line="119"/>
+        <source>fade-out</source>
+        <translation>Ausblenden</translation>
+    </message>
+    <message>
+        <location filename="../TrackSettings.cpp" line="127"/>
+        <source>player A</source>
+        <translation>Player A</translation>
+    </message>
+    <message>
+        <location filename="../TrackSettings.cpp" line="134"/>
+        <source>player B</source>
+        <translation>Player B</translation>
+    </message>
+    <message>
+        <location filename="../TrackSettings.cpp" line="175"/>
+        <source>gap</source>
+        <translation>Lücke</translation>
+    </message>
+    <message>
+        <location filename="../TrackSettings.cpp" line="177"/>
+        <location filename="../TrackSettings.cpp" line="183"/>
+        <source>s</source>
+        <translation>s</translation>
+    </message>
+    <message>
+        <location filename="../TrackSettings.cpp" line="181"/>
+        <source>random up to</source>
+        <translation>zufällig bis</translation>
+    </message>
+</context>
+<context>
+    <name>TransitionIcon</name>
+    <message>
+        <location filename="../TransitionIcon.cpp" line="34"/>
+        <source>loop with fade-out/in</source>
+        <translation>Schleife mit Ein/Aus-blenden</translation>
+    </message>
+    <message>
+        <location filename="../TransitionIcon.cpp" line="37"/>
+        <source>loop with cross-fade</source>
+        <translation>Schleife mit Überblenden</translation>
+    </message>
+    <message>
+        <location filename="../TransitionIcon.cpp" line="40"/>
+        <source>loop with gap</source>
+        <translation>Schleife mit Lücke</translation>
+    </message>
+</context>
+<context>
+    <name>Volume</name>
+    <message>
+        <location filename="../Volume.cpp" line="10"/>
+        <source>Volume: %1 dB</source>
+        <translation>Wiedergabepegel: %1 dB</translation>
+    </message>
+</context>
+</TS>
